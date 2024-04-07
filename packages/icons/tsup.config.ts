6368cfbf9d7262ae1606f5c5fc7fcf7ed592ffec
Zach Nugent <mrzachnugent@gmail.com>
2024-04-07 14:51:42 -0400

    feat: minify build output

--- packages/icons/tsup.config.ts
+++ packages/icons/tsup.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig, Options } from "tsup";
+import { defineConfig, Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
   entry: {
-    index: "src/index.tsx",
+    index: 'src/index.tsx',
   },
   banner: {
     js: "'use client'",
   },
   clean: true,
-  format: ["cjs", "esm"],
-  external: ["react"],
+  format: ['cjs', 'esm'],
+  external: ['react'],
   dts: true,
+  minify: true,
   ...options,
 }));
